fa21e3e384349c1f7ea475a34de2b21e341f3c42
travisrego <ms.anitarego@gmail.com>
2024-08-31 17:12:55 +0300

    Learning about AnchorLayout

--- firstApp/main.py
+++ firstApp/main.py
@@ -1,18 +1,22 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.boxlayout import BoxLayout
+# from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 
 
-class AnchorLayoutExample(AnchorLayout):
-    pass
+# class AnchorLayoutExample(AnchorLayout):
+#     pass
+#
+#
+# class BoxLayoutExample(BoxLayout):
+#     pass
+#
+#
+# class FirstAppWidget(Widget):
+#     pass
 
 
-class FirstAppWidget(Widget):
-    pass
-
-
-class BoxLayoutExample(BoxLayout):
+class LoginPage(AnchorLayout):
     pass
 
 
