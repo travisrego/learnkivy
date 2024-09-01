b006d9ef05ca517282e6e1e1b31480b83b0cf369
travisrego <ms.anitarego@gmail.com>
2024-09-01 13:53:05 +0300

    Learning about AnchorLayout

--- firstApp/main.py
+++ firstApp/main.py
@@ -1,22 +1,37 @@
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
-# from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 
 
-# class AnchorLayoutExample(AnchorLayout):
-#     pass
-#
-#
-# class BoxLayoutExample(BoxLayout):
-#     pass
-#
-#
-# class FirstAppWidget(Widget):
-#     pass
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+
+class FirstAppWidget(Widget):
+    pass
 
 
 class LoginPage(AnchorLayout):
+    pass
+
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for i in range(1, 50):
+            btn = Button(text="Button {0}".format(i), size_hint=(.2, .1))
+            self.add_widget(btn)
+
+
+class GridLayoutExample(GridLayout):
     pass
 
 
