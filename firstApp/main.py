128eb2ec2d691e9c8effa6bc49524d44b966f8d6
travisrego <ms.anitarego@gmail.com>
2024-09-03 16:50:46 +0300

    Learning about GridLayout, StackLayout and ScrollView

--- firstApp/main.py
+++ firstApp/main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.anchorlayout import AnchorLayout
@@ -26,8 +27,9 @@ class LoginPage(AnchorLayout):
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        for i in range(1, 50):
-            btn = Button(text="Button {0}".format(i), size_hint=(.2, .1))
+        for i in range(1, 101):
+            btn = Button(text="Button {0}".format(i), size_hint=(None, None), size=(dp(200), dp(200)), pos_hint={
+                'center_x': 0.5, 0.5: dp(200), 'center_y': 0.5})
             self.add_widget(btn)
 
 
